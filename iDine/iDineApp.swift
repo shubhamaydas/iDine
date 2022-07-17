40391cf67fd52c9d5bceb128b8a5a7eeef82c6f8
Shubhamay <shubhamaydas1@gmail.com>
2022-07-17 12:00:07 +0200

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Shubhamay Das on 17.07.22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
